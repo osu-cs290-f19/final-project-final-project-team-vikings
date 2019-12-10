9b5eef4ef19f5e17636e6cc2c11467295fa485de
GodOfAllBeings <36410886+GodOfAllBeings@users.noreply.github.com>
2019-12-10 14:00:03 -0700

    index.js

--- public/index.js
+++ public/index.js
@@ -13,36 +13,32 @@ var driveDistance = document.getElementById('question-2-followup');
 var meatFrequency = document.getElementById('question-3-followup');
 var flyFrequency = document.getElementById('question-4-followup');
 
-
-if(eatAnimals[0].checked){
-	meatFrequency.style.display = 'block';
-}
-if(flying[0].checked){
-	flyFrequency.style.display = 'block';
-}
-
+//eventlistener
 document.getElementById("question-2").addEventListener('input', question2Function);
 document.getElementById("question-3").addEventListener('input', question3Function);
 document.getElementById("question-4").addEventListener('input', question4Function);
 
-
+//function call on event handler
 function question2Function(){
 	if(drive[0].checked){
-		console.log("==Im printing from inside this function2");
-		driveDistance.style.display = 'inline';
+		console.log("==Im printing from inside this function2"); // print
+		driveDistance.style.display = 'block'; 					 //try and display the second question
+		document.getElementById('question-2').style.display = 'none'; 	//hide the initial question
 	}
 }
 
 function question3Function(){
-	if(drive[0].checked){
+	if(eatAnimals[0].checked){
 		console.log("==Im printing from inside this function3");
 		meatFrequency.style.display = 'block';
+		document.getElementById('question-3').style.display = 'none';
 	}
 }
 
 function question4Function(){
-	if(drive[0].checked){
+	if(flying[0].checked){
 		console.log("==Im printing from inside this function4");
 		flyFrequency.style.display = 'block';
+		document.getElementById('question-4').style.display = 'none';
 	}
 }
