b96ff43f01497711662ab181b4d8b6eda5c84d94
GodOfAllBeings <36410886+GodOfAllBeings@users.noreply.github.com>
2019-12-11 13:09:49 -0700

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,6 +1,7 @@
 /*
 	client side js
  */
+ var numQuestions = 7 + 1;
 //var initialQuestions = document.getElementsByClassName('question');
 var groupOfFieldset = document.getElementsByClassName('filter-fieldset'); //array of groups of checkboxes (yes/no)=1place
 //content
@@ -43,11 +44,39 @@ function calcGreen(){
 		scoreBody[i].style.display = 'block';
 		modal[i].style.display = 'block';
 	}
-	var amountOfStars = 6 - holder/5;
+	
+	if(holder == 0){
+	var amountOfStars = 5;
+	} else {
+		var amountOfStars = parseInt(numQuestions - holder/5);
+	}
+	var howGreenText;
 
 	for(var i = 0; i < amountOfStars; i++){
 		stars[i].style.display = 'inline';
 	}
+	
+	switch(amountOfStars){
+		case 1: 
+			howGreenText = "you have got 1 star, you really should consider how you can help save the environment";
+			break;
+		case 2:
+			howGreenText = "you have got 2 stars, you are doing alittle for the environment, but you should try and help more";
+			break;
+		case 3:
+			howGreenText = "you have got 3 stars, you are average in terms of helping the environment.";
+			break;
+		case 4:
+			howGreenText = "you have got 4 stars, you are above average in terms of helping the environment";
+			break;
+		case 5:
+			howGreenText = "you have got 5 stars, you are doing the best you can for the environment, keep up the good work";
+			break;
+		default:
+			howGreenText = "you have probably been saying no to everything, a true environment-savior";
+			break;
+	}
+	alert("how green are you? \n" + howGreenText);
 }
 
 buttonCloseScore.addEventListener('click', function(){
