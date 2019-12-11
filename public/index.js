457656e996773b5aeea35e39c53d185d6ddeec8e
GodOfAllBeings <36410886+GodOfAllBeings@users.noreply.github.com>
2019-12-11 13:19:37 -0700

    Update index.js

--- public/index.js
+++ public/index.js
@@ -73,7 +73,7 @@ function calcGreen(){
 			howGreenText = "you have got 5 stars, you are doing the best you can for the environment, keep up the good work";
 			break;
 		default:
-			howGreenText = "you have probably been saying no to everything, a true environment-savior";
+			howGreenText = "Cant calculate your score";
 			break;
 	}
 	alert("how green are you? \n" + howGreenText);
